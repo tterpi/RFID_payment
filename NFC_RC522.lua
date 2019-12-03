2244f8cf5a8e740c50f4b9318c6b3db4560644d0
tterpi <the.o@live.de>
2019-12-03 14:24:34 +0100

    Authentication with key B and test LED.
    When the correct id is written to block 4 of the card the led lights up for 5
    seconds. Key B is used to authenticate sector 2.

--- NFC_RC522.lua
+++ NFC_RC522.lua
@@ -428,8 +428,8 @@ function write_block(auth_mode, block_address, data)
 			  if err then print("ERROR Writing to the Tag") end
 
 			-- Read card data
-			  err, tagData = RC522.readTag(block_address)
-			  if not err then print("READ Block "..block_address..": "..appendHex(tagData)) end
+			  --err, tagData = RC522.readTag(block_address)
+			  --if not err then print("READ Block "..block_address..": "..appendHex(tagData)) end
 		  end
 
       else
@@ -454,76 +454,13 @@ function write_block(auth_mode, block_address, data)
 	return "wrote to block"
 end
 
-----------------------------------------------------------------------
--- Main
-----------------------------------------------------------------------
-
--- Initialise the RC522
-spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 0)
-gpio.mode(pin_rst,gpio.OUTPUT)
-gpio.mode(pin_ss,gpio.OUTPUT)
-gpio.write(pin_rst, gpio.HIGH)      -- needs to be HIGH all the time for the RC522 to work
-gpio.write(pin_ss, gpio.HIGH)       -- needs to go LOW during communications
-RC522.dev_write(0x01, mode_reset)   -- soft reset
-RC522.dev_write(0x2A, 0x8D)         -- Timer: auto; preScaler to 6.78MHz
-RC522.dev_write(0x2B, 0x3E)         -- Timer 
-RC522.dev_write(0x2D, 30)           -- Timer
-RC522.dev_write(0x2C, 0)            -- Timer
-RC522.dev_write(0x15, 0x40)         -- 100% ASK
-RC522.dev_write(0x11, 0x3D)         -- CRC initial value 0x6363
--- turn on the antenna
-current = RC522.dev_read(reg_tx_control)
-if bit.bnot(bit.band(current, 0x03)) then
-    RC522.set_bitmask(reg_tx_control, 0x03)
+function blocksIdentical(blockA, blockB)
+	for i,v in ipairs(blockA) do
+		if not (blockA[i] == blockB[i]) then
+			return false
+		end
+	end
+	return true
 end
 
-print("RC522 Firmware Version: 0x"..string.format("%X", RC522.getFirmwareVersion()))
-
-mtmr = tmr.create()
-mtmr:register(2000, tmr.ALARM_AUTO, function (t)
-    isTagNear, cardType = RC522.request()
-  
-    if isTagNear == true then
-      mtmr:stop()
-      err, serialNo = RC522.anticoll()
-      print("Tag Found: "..appendHex(serialNo).."  of type: "..appendHex(cardType))
-
-      -- Selecting a tag, and the rest afterwards is only required if you want to read or write data to the card
-    
-      err, sak = RC522.select_tag(serialNo)
-      if err == false then
-        print("Tag selected successfully.  SAK: 0x"..string.format("%X", sak))
-		--i is block address
-		local block_addr = 2
-        err = RC522.card_auth(auth_a, block_addr, keyA, serialNo)     --  Auth the "A" key.  if this fails you can also auth the "B" key
-        if err then 
-          print("ERROR Authenticating block "..block_addr) 
-        else 
-          -- Read card data
-            err, tagData = RC522.readTag(block_addr)
-            if not err then print("READ Block "..block_addr..": "..appendHex(tagData)) end
-        end
-      else
-        print("ERROR Selecting tag")
-    
-      end
-      print(" ")
-    
-      -- halt tag and get ready to read another.
-      buf = {}
-      buf[1] = 0x50  --MF1_HALT
-      buf[2] = 0
-      crc = RC522.calculate_crc(buf)
-      table.insert(buf, crc[1])
-      table.insert(buf, crc[2])
-      err, back_data, back_length = RC522.card_write(mode_transrec, buf)
-      RC522.clear_bitmask(0x08, 0x08)    -- Turn off encryption
-      
-     mtmr:start()
-      
-    else 
-      --print("NO TAG FOUND")
-    end
-end)  -- timer
-
-mtmr:start()
+dofile("main.lua")
--- README.md
+++ README.md
@@ -2,3 +2,16 @@ WIP: RFID payment application for public washing machines
 
 This uses an ESP8266 with NodeMCU firmware and a RC522 RFID reader.
 Based on work by limbo666 and capella-ben.
+
+In order for this script to work with your Mifare card, you have to write the correct sector trailer for sector 2 (block 7) and also the correct key to block 4.
+The access bits in the sector trailer have the effect that you can only read and write when providing key B that is assumed to be secret. This applies to data blocks and key A and B.
+
+You can configure your card with the following calls, make shure to disable the main script first, by commenting out mtmr:start() at the end of the script.
+
+-- 1. Execute the following call to write the sector trailer:
+=write_block(auth_a, 7, { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0F, 0x00, 0xFF, 0xFF, 0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56 })
+-- 2. Remove the card from the reader and place it on the reader again and then execute:
+keyB = { 0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56 }
+=write_block(auth_b, 4, { 0xAB, 0xCD, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 })
+
+This script also uses a test LED connected to D8 and GND.
