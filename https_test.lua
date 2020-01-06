b93423b11c47c0d233a7d36e808abd9d6def9f23
tterpi <the.o@live.de>
2020-01-06 01:58:09 +0100

    Updated the readme with project documentation.

--- https_test.lua
+++ https_test.lua
@@ -5,8 +5,7 @@ path = string.format("/nfcproject/LaundryRoom/1.0.1/machine/%s/hold",machineId)
 params = string.format("?userId=%s&machineId=%s&time=%s",userId, machineId, time)
 host = "virtserver.swaggerhub.com"
 
-request = string.format("POST %s%s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n", path, params, host)
-
+print("http post:")
 http.post('http://'..host..path..params,
   '',
   '',
@@ -17,16 +16,3 @@ http.post('http://'..host..path..params,
       print(code, data)
     end
   end)
-
-srv = net.createConnection(net.TCP, 0)
-srv:on("receive", function(sck, c) print("received: ") print(c) end)
-srv:on("dns", function(sck, c) print("dns: ") print(c) end)
-srv:on("reconnection", function(sck, c) print("error: ") print(c) end)
-srv:on("disconnection", function(sck, c) print("disconnect: ") print(c) end)
-srv:on("connection", function(sck, c)
-  -- Wait for connection before sending.
-  print("connected to server")
-  sck:send(request)
-end)
-print("trying to connect to server")
---srv:connect(80,host)
